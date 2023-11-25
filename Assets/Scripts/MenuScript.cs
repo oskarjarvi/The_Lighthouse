2067fdd23f5eee76b35dca0058ec65314eddd7dd
DankyDanxD <142903665+DankyDanxD@users.noreply.github.com>
2023-11-25 14:20:46 +0100

    Menu and pause menu, and some organizing

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MenuScript : MonoBehaviour
+{
+    public Animator fadeTransition;
+    public float transitionTime = 1f;
+    public void PlayGame()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        fadeTransition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+
+
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
